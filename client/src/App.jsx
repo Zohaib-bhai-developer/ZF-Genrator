63ccf309fcd1b870effe423f8b50e7b8f5cf720c
Zohaib-bhai-developer <shakeelzohaib99@gmail.com>
2025-12-05 16:38:45 +0500

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import ImageGenerator from './components/ImageGenerator';
+import CursorEffect from './components/CursorEffect';
+
+function App() {
+  return (
+    <>
+      <CursorEffect />
+      <ImageGenerator />
+    </>
+  );
+}
+
+export default App;
