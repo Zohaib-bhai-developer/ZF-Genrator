18b926828da862f33a5666216cdb67c90ac024a5
Zohaib-bhai-developer <shakeelzohaib99@gmail.com>
2025-12-05 17:16:39 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import ImageGenerator from './components/ImageGenerator';
 import CursorEffect from './components/CursorEffect';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <>
       <CursorEffect />
-      <ImageGenerator />
+      <div className="min-h-screen flex flex-col items-center justify-start pt-12 bg-gradient-to-b from-gray-900 to-black">
+        <ImageGenerator />
+        <Gallery />
+      </div>
     </>
   );
 }
