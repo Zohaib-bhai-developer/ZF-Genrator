efa226c24d95dc90b5847702b7dd030f906813a7
Zohaib-bhai-developer <shakeelzohaib99@gmail.com>
2025-12-05 17:17:48 +0500

    Update Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 export default function Loader() {
   return (
     <motion.div
-      className="w-16 h-16 border-4 border-t-blue-500 border-gray-300 rounded-full"
+      className="w-16 h-16 border-4 border-t-blue-500 border-gray-700 rounded-full"
       animate={{ rotate: 360 }}
       transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
     />
